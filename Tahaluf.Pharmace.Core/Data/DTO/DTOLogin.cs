d182c6fdb8ef1e7639092fe18527013fa9fc0da7
BHRE213 <batoolmershed212@hotmail.com>
2022-06-04 21:57:41 +0300

    hgfgc

---
+++ Tahaluf.Pharmace.Core/Data/DTO/DTOLogin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tahaluf.Pharmace.Core.Data.DTO
+{
+   public class DTOLogin
+    {
+        public string Username { get; set; }
+        public string Rolename { get; set; }
+    }
+}
